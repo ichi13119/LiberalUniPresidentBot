fb774915c135dc8cb3e1f3f7d6608b233adb8676
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2019-10-10 23:45:53 +0900

    videosテーブルの作成

---
+++ src/liveral-uni-president-bot/database/migrations/2019_10_10_142717_create_videos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 200); //Youtube動画タイトル（最大全角100文）
+            $table->string('url', 2048); //Youtube動画URL（IEのURL最大文字数2048字）
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
