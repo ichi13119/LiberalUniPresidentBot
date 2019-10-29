24cf1a9b9075bccd65410739032626fd7c7c37ff
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2019-10-30 00:09:27 +0900

    VideoとImportantWordのリレーションの作成および出現頻度の計算ロジック作成

--- src/liveral-uni-president-bot/app/Models/Video.php
+++ src/liveral-uni-president-bot/app/Models/Video.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     //
+    public function importantWords()
+    {
+        return $this->hasMany('App\Models\ImportantWord');
+    }
+
 }
--- src/liveral-uni-president-bot/app/Observers/VideoObserver.php
+++ src/liveral-uni-president-bot/app/Observers/VideoObserver.php
@@ -24,8 +24,55 @@ class VideoObserver
      */
     public function saved(Video $video)
     {
-        //\Log::debug($video->subtitles);
-        \Log::debug(preg_replace('/(\d{2}:\d{2})(\n|\r\n|\r)/', '',$video->subtitles));
+        $video->subtitles = preg_replace('/(\d{2}:\d{2})(\n|\r\n|\r)/', '',$video->subtitles);
+        $mecab = new \Mecab\Tagger();
+        $nodes = $mecab->parseToNode($video->subtitles);
+        $words = array();
+        $isCompoundNoun = false;
+        $compoundNoun = '';
+        foreach ($nodes as $n) {
+
+            $result = explode(',', $n->getFeature());
+
+            if($n->getSurface() == '')
+                continue;
+
+            if($isCompoundNoun == false && $result[0] != '名詞'){
+                $compoundNoun = '';
+                continue;
+            }else if($isCompoundNoun && $result[0] != '名詞'){
+                if(empty($words[$compoundNoun])){
+                    $words[$compoundNoun] = 0;
+                }
+                $words[$compoundNoun]++;
+                $compoundNoun = '';
+                $isCompoundNoun = false;
+                continue;
+            }else if($isCompoundNoun && $result[0] == '名詞'){
+                $compoundNoun .= $n->getSurface();
+            }else{
+                $compoundNoun .= $n->getSurface();
+                $isCompoundNoun = true;
+            }
+
+            if(empty($words[$n->getSurface()])){
+                $words[$n->getSurface()] = 0;
+            }
+            $words[$n->getSurface()]++;
+        }
+        //$words = array_filter($words, 'isThresholdValueOrMore');
+        arsort($words);
+
+        foreach ($words as $key => $ranking) {
+            $importantWord = new \App\Models\ImportantWord();
+            $importantWord->word = $key;
+            $importantWord->ranking = $ranking;
+            $video->importantWords()->save($importantWord);
+        }
+
+        \Log::debug(var_dump($words) . PHP_EOL);
+
+
     }
 
     /**
