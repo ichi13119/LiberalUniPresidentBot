66b0f1a85abe7b547ed8b52691256613f61f5472
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2019-11-08 01:53:38 +0900

    delete:#1 不要なファイルを削除

--- src/mecab_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str = 'すもももももももものうち';
-
-$mecab = new \Mecab\Tagger();
-$nodes = $mecab->parseToNode($str);
-foreach ($nodes as $n) {
-    $items = $n->getFeature();
-    echo $items . PHP_EOL;
-}
