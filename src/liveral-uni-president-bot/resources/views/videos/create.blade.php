9c99b41047d3e27bfa0877c4eb26b6d2f2873992
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2019-11-20 00:40:28 +0900

    update:#11 動画追加フォームにてURL指定するだけでタイトルとサムネイルを取得する

--- src/liveral-uni-president-bot/resources/views/videos/create.blade.php
+++ src/liveral-uni-president-bot/resources/views/videos/create.blade.php
@@ -20,4 +20,40 @@
             </div>
         </div>
     </div>
-    @endsection
+    <script type="text/javascript">
+        $('#url').change('click',function(){
+
+            $videoId = youtube_parser($(this).val());
+
+            if(!$videoId){
+                return;
+            }
+
+            $.ajax({
+                url:'/api/youtube/getVideoInformation?videoId=' + $videoId,
+                type:'GET',
+                dataType:"json",
+            })
+            // Ajaxリクエストが成功した時発動
+            .done( (data) => {
+                setVideoInformation(data);
+            })
+            // Ajaxリクエストが失敗した時発動
+            .fail( (data) => {
+            });
+        });
+
+        function youtube_parser(url){
+            var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+            var match = url.match(regExp);
+            return (match&&match[7].length==11)? match[7] : false;
+        }
+
+        function setVideoInformation(data){
+            $('#title').val(data['snippet']['title']);
+            $('#thumbnail').empty();
+            let imageTag = '<img src="' + data['snippet']['thumbnails']['medium']['url'] + '">';
+            $('#thumbnail').append(imageTag);
+        }
+    </script>
+@endsection
